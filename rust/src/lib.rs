5fb11ca436711cd0acf544aabfefd66f60f8bb78
juyufeng <345108530@qq.com>
2025-05-10 19:53:12 +0800

    移除Rust插件代码中的冗余注释，简化hello_world函数，保持代码整洁。

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,15 +1,5 @@
-//! car-hello-world Wasm插件
-//! 这是一个基于Extism框架的示例插件，用于演示WebAssembly与React+Electron的集成
-
 use extism_pdk::*;
 
-/// 插件的主要函数，返回一个问候消息
-/// 
-/// # 返回值
-/// 
-/// 返回一个包含问候语的字符串
-#[plugin_fn]
 pub fn hello_world() -> FnResult<String> {
-    // 返回一个简单的问候消息
     Ok(String::from("Hello, car-hello-world!"))
 }
